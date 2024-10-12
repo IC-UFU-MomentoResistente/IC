03d61ba312f00f31efbe5accd4771f5c5eeaa0b9
Gabriel Lunarti <gabriellunarti@gmail.com>
2024-10-11 21:31:58 -0300

    Código 10/11/2024
    Co-authored-by: Arthur Cunha Pena <Arthur1510@users.noreply.github.com>

--- window.cpp
+++ window.cpp
@@ -11,7 +11,8 @@
 #include <stdio.h>
 #include "data_storage.h"
 
-struct Point {
+struct Point
+{
     float x;
     float y;
 
@@ -19,31 +20,39 @@ struct Point {
     Point(float x_val, float y_val) : x(x_val), y(y_val) {}
 };
 
-class Polygon {
+class Polygon
+{
 public:
     std::vector<Point> vertices;
-    void setVertices(const std::vector<Point>& points) {
+    std::vector<Point> resultadoCorte;
+    void setVertices(const std::vector<Point> &points)
+    {
         vertices.clear();
-        for (const auto& p : points) {
+        for (const auto &p : points)
+        {
             vertices.emplace_back(p.x, p.y);
         }
     }
 
-    double area() const {
+    double area() const
+    {
         double A = 0.0;
         int n = vertices.size();
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             int j = (i + 1) % n;
             A += vertices[i].x * vertices[j].y - vertices[j].x * vertices[i].y;
         }
         return std::abs(A) / 2.0;
     }
 
-    Point centroid() const {
+    Point centroid() const
+    {
         double Cx = 0.0, Cy = 0.0;
         double A = area();
         int n = vertices.size();
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             int j = (i + 1) % n;
             double factor = (vertices[i].x * vertices[j].y - vertices[j].x * vertices[i].y);
             Cx += (vertices[i].x + vertices[j].x) * factor;
@@ -53,51 +62,56 @@ public:
         Cy /= (6.0 * A);
         return Point(Cx, Cy);
     }
-    
-    int verificarCaso(const Point& p1, const Point& p2, double cortar) {
-        if ((p2.y <= cortar && p1.y >= cortar) || (p2.y >= cortar && p1.y <= cortar)) {
+
+    int verificarCaso(const Point &p1, const Point &p2, double cortar)
+    {
+        if ((p2.y <= cortar && p1.y >= cortar) || (p2.y >= cortar && p1.y <= cortar))
+        {
             return 2; // Caso 2: Corta o trecho
         }
         return 1; // Caso 1: Adiciona o nó final na nova poligonal
     }
 
     // Função para calcular o ponto de interseção
-    Point calcularIntersecao(const Point& p1, const Point& p2, double cortar) {
+    Point calcularIntersecao(const Point &p1, const Point &p2, double cortar)
+    {
         double deltaY = p2.y - p1.y;
         double deltaX = p2.x - p1.x;
-        
-        if (std::abs(deltaX) < 1e-6) {
+
+        if (std::abs(deltaX) < 1e-6)
+        {
             return Point(p1.x, cortar);
-        } else {
+        }
+        else
+        {
             double tg = deltaY / deltaX;
             double x = p1.x - (p1.y - cortar) / tg;
             return Point(x, cortar);
         }
     }
 
-    std::vector<Point> resultadoCorte = vertices;
-    std::vector<Point> cortarPoligonal(const std::vector<Point>& vertices, const std::vector<float>& cortar) {
+    void cortarPoligonal(const std::vector<Point> &vertices, float &cortar)
+    {
+        float nivel = cortar;
+        std::vector<Point> novaPoligonal;
+        int nv = vertices.size();
 
-        for (float nivel : cortar) {
-            std::vector<Point> novaPoligonal;
-            int nv = resultadoCorte.size();
+        for (int i = 0; i < nv; i++)
+        {
+            int caso = verificarCaso(vertices[i], vertices[(i + 1) % nv], nivel);
 
-            for (int i = 0; i < nv; i++) {
-                int caso = verificarCaso(resultadoCorte[i], resultadoCorte[(i + 1) % nv], nivel);
-
-                if (caso == 2) {
-                    Point intersecao = calcularIntersecao(resultadoCorte[i], resultadoCorte[(i + 1) % nv], nivel);
-                    novaPoligonal.push_back(intersecao);
-                }
-
-                novaPoligonal.push_back(resultadoCorte[(i + 1) % nv]);
+            if (caso == 2)
+            {
+                Point intersecao = calcularIntersecao(vertices[i], vertices[(i + 1) % nv], nivel);
+                novaPoligonal.push_back(intersecao);
             }
-            resultadoCorte = novaPoligonal;
+
+            novaPoligonal.push_back(vertices[(i + 1) % nv]);
         }
-        return resultadoCorte;
+
+        resultadoCorte = novaPoligonal;
     }
 };
-
 
 // Variáveis globais
 std::vector<Point> collectedPoints; // Armazenar os pontos coletados
@@ -116,23 +130,24 @@ void IniciarInterface()
 
 void loopPrograma()
 {
-    float VLN = 0;
-    std::vector<float> cortar;
+    static float VLN = 0;
+    static float cortar = 0;
     static int tempNumPoints = 0;
     static bool showGraficoWindow = true;
     static bool showDadosWindow = true;
-    static bool showDadosWindowTwo = true; 
-    
+    static bool showDadosWindowTwo = true;
+
     while (!WindowShouldClose())
     {
-       
+
         BeginDrawing();
         ClearBackground(BLACK);
         rlImGuiBegin();
 
         // Janela de Inserção de Dados
-        if (showDadosWindow) {
-            ImGui::Begin("Inserir Dados", &showDadosWindow);  // Título da janela
+        if (showDadosWindow)
+        {
+            ImGui::Begin("Inserir Dados", &showDadosWindow); // Título da janela
 
             ImGui::Text("DADOS");
             ImGui::InputInt("Número de Pontos", &tempNumPoints);
@@ -165,79 +180,81 @@ void loopPrograma()
                 ImGui::EndTable();
             }
 
-                       
-            ImGui::End();  // Finaliza a janela de dados
+            ImGui::End(); // Finaliza a janela de dados
         }
 
-            if (showDadosWindowTwo) {
-                ImGui::Begin("Central de operações com polígono", &showDadosWindowTwo);
-                ImGui::Text("Insira a coordenada de corte");
+        if (showDadosWindowTwo)
+        {
+            ImGui::Begin("Central de operações com polígono", &showDadosWindowTwo);
+            ImGui::Text("Insira a coordenada de corte");
 
-                // cortar.clear(); // Limpa o vetor de cortes antes de adicionar novos valores
+            // cortar.clear(); // Limpa o vetor de cortes antes de adicionar novos valores
 
-                // Declare VLN fora do loop, se ainda não estiver declarado
-                static float VLN = 0.0f; // Usar static para manter o valor entre as chamadas
+            // Declare VLN fora do loop, se ainda não estiver declarado
+            // Usar static para manter o valor entre as chamadas
 
-                ImGui::InputFloat("Y", &VLN); // Permite ao usuário inserir a coordenada Y
+            ImGui::InputFloat("Y", &VLN); // Permite ao usuário inserir a coordenada Y
 
-                // Adiciona a coordenada Y ao vetor de cortes
-                if (ImGui::Button("Adicionar Corte")) { // Botão para adicionar o corte
-                    cortar.push_back(VLN);
-                }
-                
-                ImGui::Text("Cortes adicionados:");
-                for (const auto& corte : cortar) {
-                        ImGui::Text("%.2f", corte); // Mostra cada corte adicionado
-                }
+            // Adiciona a coordenada Y ao vetor de cortes
+            if (ImGui::Button("Adicionar Corte"))
+            { // Botão para adicionar o corte
+                cortar = VLN;
+            }
 
+            if (ImGui::Button("Cortar"))
+            {
+                polygon.setVertices(collectedPoints);
+                polygon.cortarPoligonal(polygon.vertices, cortar);
+            }
 
-                if(ImGui::Button("Cortar")){
-                    polygon.setVertices(collectedPoints);
-                    polygon.cortarPoligonal(polygon.vertices, cortar);
-                    int NV = polygon.vertices.size();
-                    TraceLog(LOG_INFO, "Valores dos Cortes Armazenados");
-                    for(int i = 0; i < NV; i++){
-                        TraceLog(LOG_INFO, "x = %2.f, y = %2.f", polygon.resultadoCorte[i].x, polygon.resultadoCorte[i].y);
-                    }
-                }
-            
             if (ImGui::Button("Mostrar Valores"))
             {
-                TraceLog(LOG_INFO, "VLN", VLN);
-                TraceLog(LOG_INFO, "Valor do corte", &cortar[0]);
+
                 TraceLog(LOG_INFO, "Valores armazenados:");
-                for (const auto& point : collectedPoints)
+                for (const auto &point : collectedPoints)
                 {
-                    TraceLog(LOG_INFO, "x = %.2f, y = %.2f", point.x, point.y);             
+                    TraceLog(LOG_INFO, "x = %.2f, y = %.2f", point.x, point.y);
                 }
+                TraceLog(LOG_INFO, "Valores após o corte");
+                for (const auto &point : polygon.resultadoCorte)
+                {
+                    TraceLog(LOG_INFO, "x = %.2f, y = %.2f", point.x, point.y);
+                }
+                TraceLog(LOG_INFO, "Corte", cortar);
             }
-            
+
             if (ImGui::Button("Calcular Área e Centróide"))
             {
-                polygon.setVertices(collectedPoints);  // Transfere os pontos para o polígono
-                 // Garante o sentido anti-horário
-
-                double polygonArea = polygon.area();    // Calcula a área
-                Point centroid = polygon.centroid();    // Calcula o centróide
+                polygon.setVertices(collectedPoints); // Transfere os pontos para o polígono
+                double polygonArea = polygon.area();  // Calcula a área
+                Point centroid = polygon.centroid();  // Calcula o centróide
 
                 TraceLog(LOG_INFO, "Área: %.2f", polygonArea);
                 TraceLog(LOG_INFO, "Centróide: (%.2f, %.2f)", centroid.x, centroid.y);
-
             }
 
-                                 
-                ImGui::End();
-            
-            }
+            ImGui::End();
+        }
 
         // Janela do Gráfico
-        if (showGraficoWindow) {
-            ImGui::Begin("Gráfico da Seção Transversal", &showGraficoWindow);  // Título da janela
+        if (showGraficoWindow)
+        {
+            ImGui::Begin("Gráfico da Seção Transversal", &showGraficoWindow); // Título da janela
 
-            static float x_data[100];
-            static float y_data[100];
+            static float x_values[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}; // teste
+            static float y_values[11];
+
+            for (int i = 0; i < sizeof(x_values); i++)
+            {
+                y_values[i] = VLN;
+            }
+
             int numPoints = collectedPoints.size();
+            float x_corte[polygon.resultadoCorte.size()];
+            float y_corte[polygon.resultadoCorte.size()];
 
+            float x_data[collectedPoints.size()];
+            float y_data[collectedPoints.size()];
 
             if (numPoints >= 3) // Verifica se foram inseridos pelo menos 3 vértices
             {
@@ -248,6 +265,12 @@ void loopPrograma()
                     y_data[i] = collectedPoints[i].y;
                 }
 
+                for (const auto &point : polygon.resultadoCorte)
+                {
+                    x_corte[i] = polygon.resultadoCorte[i].x;
+                    y_corte[i] = polygon.resultadoCorte[i].y;
+                }
+
                 // Adiciona o primeiro ponto ao final para fechar o polígono
                 x_data[numPoints] = collectedPoints[0].x;
                 y_data[numPoints] = collectedPoints[0].y;
@@ -256,7 +279,9 @@ void loopPrograma()
                 if (ImPlot::BeginPlot("Gráfico"))
                 {
                     ImPlot::PlotScatter("Vértices", x_data, y_data, numPoints);
-                    ImPlot::PlotLine("Polígono", x_data, y_data, numPoints + 1);  // Aumente para numPoints + 1
+                    ImPlot::PlotScatter("Vértices cortadas", x_corte, y_corte, numPoints);
+                    ImPlot::PlotLine("Polígono", x_data, y_data, numPoints + 1); // Aumente para numPoints + 1
+                    ImPlot::PlotLine("Linha de corte,", x_values, y_values, 6);
                     ImPlot::EndPlot();
                 }
             }
@@ -265,7 +290,7 @@ void loopPrograma()
                 ImGui::Text("Insira pelo menos 3 vértices para formar um polígono.");
             }
 
-            ImGui::End();  // Finaliza a janela do gráfico
+            ImGui::End(); // Finaliza a janela do gráfico
         }
 
         rlImGuiEnd();
@@ -276,12 +301,3 @@ void loopPrograma()
     ImPlot::DestroyContext();
     rlImGuiShutdown();
 }
-
-
-
-// Colocar uma caixa de texto para o usuário inserir a coordenada y de corte
-// Botão para mostrar a linha de corte antes de cortar
-
-// Botão que corta o polígono
-
-// Janela que mostra o polígono comprimido

ca04d4a0df8a8751ec05aa65197fd3e526fea9df
Gabriel Lunarti <gabriellunarti@gmail.com>
2024-10-13 10:47:07 -0300

    Dia 13/10/2024

--- window.cpp
+++ window.cpp
@@ -309,19 +309,6 @@ void loopPrograma()
 // Problemas: Verificar ---
 // Quando acrescenta uma certa quantidade de pontos, o primeiro ponto some, ai tem que inserir mais um vetor pra preencher o lugar
 // do primeiro vetor
-
-// Tarefas
-// Definir seção I pronta para começar a interface
-// Inserir pontos de corte na janela do gráfico
-// Ajustar tamanho dos vetores x_data, y_data, x_corte, y_corte
-// Ajustar desenho da linha com os limites de x + 10%
-// Funções para corte superior e corte inferior
-// Corta com as setas do teclado a cada frame ou a cada "clique"
-// Rodas a seção com as setas
-
-// Problemas: Verificar ---
-// Quando acrescenta uma certa quantidade de pontos, o primeiro ponto some, ai tem que inserir mais um vetor pra preencher o lugar
-// do primeiro vetor
 // Tarefas
 // Definir seção I pronta para começar a interface
 // Inserir pontos de corte na janela do gráfico

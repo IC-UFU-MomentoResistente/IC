a9b750563131b8447b16f0163841bf191a1dd758
Gabriel Lunarti <gabriellunarti@gmail.com>
2024-10-22 16:25:48 -0300

    22

--- window.cpp
+++ window.cpp
@@ -11,7 +11,8 @@
 #include <stdio.h>
 #include <set>
 
-struct Point {
+struct Point
+{
     float x;
     float y;
 
@@ -19,8 +20,10 @@ struct Point {
     Point(float x_val, float y_val) : x(x_val), y(y_val) {}
 
     // Definir o operador de comparação para que o std::set funcione
-    bool operator<(const Point& other) const {
-        if (x != other.x) {
+    bool operator<(const Point &other) const
+    {
+        if (x != other.x)
+        {
             return x < other.x;
         }
         return y < other.y;
@@ -34,7 +37,7 @@ public:
     std::vector<Point> resultadoCorte;
     std::vector<Point> AreaSuperior;
     std::vector<Point> AreaInferior;
-    
+
     void setVertices(const std::vector<Point> &points)
     {
         vertices.clear();
@@ -46,13 +49,16 @@ public:
 
     int verificarCaso(const Point &p1, const Point &p2, double cortar)
     {
-        if ((p2.y <= cortar && p1.y >= cortar) || (p2.y >= cortar && p1.y <= cortar)){
+        if ((p2.y <= cortar && p1.y >= cortar) || (p2.y >= cortar && p1.y <= cortar))
+        {
             return 2; // Caso 2: Corta o trecho
         }
-        if (p2.y >= cortar && p1.y >= cortar){
+        if (p2.y >= cortar && p1.y >= cortar)
+        {
             return 3; // caso 3: vértices acima da linha de corte; área comprimida
         }
-        if (p2.y <= cortar && p1.y <= cortar){
+        if (p2.y <= cortar && p1.y <= cortar)
+        {
             return 4; // caso 4: vértices abaixo da linha de corte: área tracionada
         }
         return 1; // Caso 1: Adiciona o nó final na nova poligonal
@@ -127,8 +133,10 @@ public:
         }
     }
 
-    void fecharPoligono(std::vector<Point>& pontos) {
-        if (!pontos.empty()) {
+    void fecharPoligono(std::vector<Point> &pontos)
+    {
+        if (!pontos.empty())
+        {
             pontos.push_back(pontos[0]);
         }
     }
@@ -136,9 +144,7 @@ public:
 
 // Variáveis globais
 std::vector<Point> collectedPoints = {
-    {0,190}, {0,178}, {50,170}, {50,45}, {25,25}, {25,0},
-    {95,0}, {95, 25}, {70,45}, {70,170}, {120,178}, {120,190}
-}; // Armazenar os pontos coletados
+    {0, 190}, {0, 178}, {50, 170}, {50, 45}, {25, 25}, {25, 0}, {95, 0}, {95, 25}, {70, 45}, {70, 170}, {120, 178}, {120, 190}}; // Armazenar os pontos coletados
 Polygon polygon;
 
 // Funções de inicialização da interface
@@ -146,7 +152,7 @@ void IniciarInterface()
 {
     const int screenWidth = 1280;
     const int screenHeight = 720;
-    InitWindow(screenWidth, screenHeight, "Software de cálculo de esforços em concreto armado");
+    InitWindow(screenWidth, screenHeight, "Software de cálculo de esforços em seções de concreto armado");
     SetTargetFPS(60);
     rlImGuiSetup(true);
     ImPlot::CreateContext();
@@ -154,18 +160,34 @@ void IniciarInterface()
 
 void loopPrograma()
 {
+    float KeyDownDelay = 0.0f;
+    float KeyDownDelayTime = 0.1f;
+
+    int numBarras = 1;
     static float VLN = 0;
     static float cortar = 0;
+    static float diametroBarras = 0;
+    static float diametroPosXi = 0;
+    static float diametroPosYi = 0;
+    static float diametroPosXf = 0;
+    static float diametroPosYf = 0;
     static bool showGraficoWindow = true;
     static bool showDadosWindowTwo = true;
+    static bool tabelaArmadura = true;
+    static bool umaBarra = false;
+    static bool linhaBarras = false;
 
-    while (!WindowShouldClose()){
+    while (!WindowShouldClose())
+    {
+        DrawFPS(20, 20);
+        KeyDownDelay = KeyDownDelay + GetFrameTime();
 
         BeginDrawing();
         ClearBackground(BLACK);
         rlImGuiBegin();
 
-        if (showDadosWindowTwo){
+        if (showDadosWindowTwo)
+        {
             ImGui::Begin("Central de operações com polígono", &showDadosWindowTwo);
             ImGui::Text("Insira a coordenada de corte");
 
@@ -173,7 +195,7 @@ void loopPrograma()
 
             // Declare VLN fora do loop, se ainda não estiver declarado
             // Usar static para manter o valor entre as chamadas
-
+            ImGui::SetNextItemWidth(100);
             ImGui::InputFloat("Y", &VLN); // Permite ao usuário inserir a coordenada Y
 
             // Adiciona a coordenada Y ao vetor de cortes
@@ -203,13 +225,15 @@ void loopPrograma()
                     TraceLog(LOG_INFO, "x = %.2f, y = %.2f", point.x, point.y);
                 }
 
-                TraceLog(LOG_INFO, "Vertices comprimidos");
-                for (const auto &point : polygon.AreaSuperior){
+                TraceLog(LOG_INFO, "Vertices superiores");
+                for (const auto &point : polygon.AreaSuperior)
+                {
                     TraceLog(LOG_INFO, "x = %.2f, y = %.2f", point.x, point.y);
                 }
 
-                TraceLog(LOG_INFO, "Vertices tracionados");
-                for (const auto &point : polygon.AreaInferior){
+                TraceLog(LOG_INFO, "Vertices inferiores");
+                for (const auto &point : polygon.AreaInferior)
+                {
                     TraceLog(LOG_INFO, "x = %.2f, y = %.2f", point.x, point.y);
                 }
             }
@@ -217,55 +241,141 @@ void loopPrograma()
             ImGui::End();
         }
 
-        if (showGraficoWindow){
+        if (tabelaArmadura)
+        {
+            ImGui::Begin("Entrada de dados: Armadura Passiva", &tabelaArmadura);
+            ImGui::Checkbox("Uma Barra", &umaBarra);
+            if (umaBarra == true)
+            {
+                linhaBarras = false;
+            }
+
+            ImGui::Checkbox("Linha de Barras", &linhaBarras);
+            if (linhaBarras == true)
+            {
+                umaBarra = false;
+            }
+
+            if (umaBarra == true)
+            {
+                numBarras = 1;
+                ImGui::PushItemWidth(50);
+                ImGui::InputFloat("Diâmetro das Barras", &diametroBarras);
+                ImGui::InputFloat("Posição X (mm)", &diametroPosXi);
+                ImGui::SameLine();
+                ImGui::InputFloat("Posição Y (mm)", &diametroPosYi);
+            }
+
+            if (linhaBarras == true)
+            {
+                ImGui::SetNextItemWidth(100);
+                ImGui::InputInt("Numero de Barras na Linha", &numBarras);
+                ImGui::PushItemWidth(50);
+                ImGui::InputFloat("Diâmetro das Barras", &diametroBarras);
+                ImGui::InputFloat("Posição Xi (mm)", &diametroPosXi);
+                ImGui::SameLine();
+                ImGui::InputFloat("Posição Yi (mm)", &diametroPosYi);
+                ImGui::InputFloat("Posição Xf (mm)", &diametroPosXf);
+                ImGui::SameLine();
+                ImGui::InputFloat("Posição Yf (mm)", &diametroPosYf);
+            }
+            ImGui::Button("Adicionar");
+            ImGui::SameLine();
+            ImGui::Button("Remover");
+            if (ImGui::BeginTable("Tabela", 4, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg))
+            {
+
+                ImGui::TableSetupColumn("Barra");
+                ImGui::TableSetupColumn("Diâmetro");
+                ImGui::TableSetupColumn("X(cm)");
+                ImGui::TableSetupColumn("Y(cm)");
+                ImGui::TableHeadersRow();
+                ImGui::EndTable();
+            }
+
+            ImGui::End();
+        }
+
+        if (showGraficoWindow)
+        {
             ImGui::Begin("Gráfico da Seção Transversal", &showGraficoWindow); // Título da janela
 
             static float x_values[2]; // teste
             static float y_values[2];
 
-            float valorMenor = FLT_MAX; //Número de ponto flutuante representável máximo.
+            float valorMenor = FLT_MAX;  // Número de ponto flutuante representável máximo.
             float valorMaior = -FLT_MAX; // Número de ponto flutuante representável máximo.
-
 
             for (int i = 0; i < sizeof(x_values); i++)
             {
                 y_values[i] = VLN;
             }
 
-            if(IsKeyPressed(KEY_UP)) {
+            if (IsKeyPressed(KEY_UP))
+            {
                 VLN = VLN + 1;
-            
+
                 cortar = VLN;
                 polygon.setVertices(collectedPoints);
                 polygon.cortarPoligonal(polygon.vertices, cortar);
-            
+
+                KeyDownDelay = 0.0f;
             }
-            if(IsKeyPressed(KEY_DOWN)) {
+            if (IsKeyPressed(KEY_DOWN))
+            {
                 VLN = VLN - 1;
 
                 cortar = VLN;
                 polygon.setVertices(collectedPoints);
                 polygon.cortarPoligonal(polygon.vertices, cortar);
-            
+
+                KeyDownDelay = 0.0f;
             }
-            
+
+            if (KeyDownDelay >= KeyDownDelayTime)
+            {
+                if (IsKeyDown(KEY_UP))
+                {
+
+                    VLN = VLN + 1;
+                    cortar = VLN;
+                    polygon.setVertices(collectedPoints);
+                    polygon.cortarPoligonal(polygon.vertices, cortar);
+
+                    KeyDownDelay = 0.0f;
+                }
+                if (IsKeyDown(KEY_DOWN))
+                {
+                    VLN = VLN + -1;
+                    cortar = VLN;
+                    polygon.setVertices(collectedPoints);
+                    polygon.cortarPoligonal(polygon.vertices, cortar);
+
+                    KeyDownDelay = 0.0f;
+                }
+            }
+
             int numPoints = collectedPoints.size();
-            
-            for (int i = 0; i <= numPoints; i++){        
-                if (collectedPoints[i].x > valorMaior) {
+
+            for (int i = 0; i <= numPoints; i++)
+            {
+                if (collectedPoints[i].x > valorMaior)
+                {
                     valorMaior = collectedPoints[i].x;
                 }
-                
-                if (collectedPoints[i].x < valorMenor) {
+
+                if (collectedPoints[i].x < valorMenor)
+                {
                     valorMenor = collectedPoints[i].x;
                 }
             }
 
-            x_values[0] = valorMenor - (valorMaior-valorMenor)*0.1;
-            x_values[1] = valorMaior + (valorMaior-valorMenor)*0.1;
-            
+            x_values[0] = valorMenor - (valorMaior - valorMenor) * 0.1;
+            x_values[1] = valorMaior + (valorMaior - valorMenor) * 0.1;
+
             // Ajuste o código de desenho no gráfico
-            if (numPoints >= 3) {
+            if (numPoints >= 3)
+            {
                 // Fechar os vetores adicionando o primeiro ponto ao final
                 std::vector<Point> collectedPointsFechados = collectedPoints;
                 polygon.fecharPoligono(collectedPointsFechados);
@@ -286,22 +396,26 @@ void loopPrograma()
                 float x_inferior[AreaInferiorFechado.size()];
                 float y_inferior[AreaInferiorFechado.size()];
 
-                for (size_t i = 0; i < collectedPointsFechados.size(); i++) {
+                for (size_t i = 0; i < collectedPointsFechados.size(); i++)
+                {
                     x_data[i] = collectedPointsFechados[i].x;
                     y_data[i] = collectedPointsFechados[i].y;
                 }
 
-                for (size_t i = 0; i < resultadoCorteFechado.size(); i++) {
+                for (size_t i = 0; i < resultadoCorteFechado.size(); i++)
+                {
                     x_corte[i] = resultadoCorteFechado[i].x;
                     y_corte[i] = resultadoCorteFechado[i].y;
                 }
 
-                for (size_t i = 0; i < AreaSuperiorFechado.size(); i++) {
+                for (size_t i = 0; i < AreaSuperiorFechado.size(); i++)
+                {
                     x_superior[i] = AreaSuperiorFechado[i].x;
                     y_superior[i] = AreaSuperiorFechado[i].y;
                 }
 
-                for (size_t i = 0; i < AreaInferiorFechado.size(); i++) {
+                for (size_t i = 0; i < AreaInferiorFechado.size(); i++)
+                {
                     x_inferior[i] = AreaInferiorFechado[i].x;
                     y_inferior[i] = AreaInferiorFechado[i].y;
                 }
@@ -310,7 +424,8 @@ void loopPrograma()
                 ImVec2 plotSize = ImGui::GetContentRegionAvail();
 
                 // Plota os pontos e desenha o polígono
-                if (ImPlot::BeginPlot("Gráfico", ImVec2(plotSize.x, plotSize.y))) {
+                if (ImPlot::BeginPlot("Gráfico", ImVec2(plotSize.x, plotSize.y)))
+                {
                     ImPlot::PlotScatter("Vértices", x_data, y_data, collectedPointsFechados.size());
                     ImPlot::PlotScatter("Vértices cortadas", x_corte, y_corte, resultadoCorteFechado.size());
                     ImPlot::PlotScatter("Vértices superiores", x_superior, y_superior, AreaSuperiorFechado.size());
@@ -323,7 +438,8 @@ void loopPrograma()
                     ImPlot::EndPlot();
                 }
             }
-            else{
+            else
+            {
                 ImGui::Text("Insira pelo menos 3 vértices para formar um polígono.");
             }
 
@@ -339,12 +455,11 @@ void loopPrograma()
     rlImGuiShutdown();
 }
 
-
-//Tarefas
-//Definir seção I pronta para começar a interface FEITO 
-//Inserir pontos de corte na janela do gráfico  FEITO 
-//Ajustar tamanho dos vetores x_data, y_data, x_corte, y_corte FEITO 
-//Ajustar desenho da linha com os limites de x + 10% FEITO 
-//Funções para corte superior e corte inferior FEITO 
-//Corta com as setas do teclado a cada frame ou a cada "clique" FEITO
-//Rodar a seção com as setas - A FAZER 
+// Tarefas
+// Definir seção I pronta para começar a interface FEITO
+// Inserir pontos de corte na janela do gráfico  FEITO
+// Ajustar tamanho dos vetores x_data, y_data, x_corte, y_corte FEITO
+// Ajustar desenho da linha com os limites de x + 10% FEITO
+// Funções para corte superior e corte inferior FEITO
+// Corta com as setas do teclado a cada frame ou a cada "clique" FEITO
+// Rodar a seção com as setas - A FAZER

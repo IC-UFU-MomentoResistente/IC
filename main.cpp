594d407b11d8d9b888e443bd0bb3b0a1fcc11d53
Arthur Cunha Pena <92754074+Arthur1510@users.noreply.github.com>
2024-10-30 14:44:03 +0000

    começando as classes do concreto e do aço

--- main.cpp
+++ main.cpp
@@ -21,14 +21,13 @@ struct Point {
     }
 };
 
-class Polygon
-{
+class Poligono{
 public:
     std::vector<Point> vertices;
     std::vector<Point> resultadoCorte;
     std::vector<Point> areaComprimida;
     std::vector<Point> areaTracionada;
-    std::vector<Point> translatedVertices;
+    std::vector<Point> verticesTransladados;
 
     bool isClockwise() const {
         double sum = 0.0;
@@ -65,7 +64,7 @@ public:
         return std::abs(A) / 2.0;
     }
 
-    Point centroid() const {
+    Point centroide() const {
         double Cx = 0.0, Cy = 0.0;
         double A = area();  
         int n = vertices.size();
@@ -101,8 +100,8 @@ public:
     }
 
     std::vector<Point> translateToCG() const {
-        Point cg = centroid();
-        std::vector<Point> translatedVertices;
+        Point cg = centroide();
+        std::vector<Point> verticesTransladados;
         int n = vertices.size();
 
             for (size_t i = 0; i < n; ++i) {
@@ -111,39 +110,39 @@ public:
                 double translatedX = vertex.x - cg.x;
                 double translatedY = vertex.y - cg.y;
 
-                translatedVertices.emplace_back(translatedX, translatedY);
+                verticesTransladados.emplace_back(translatedX, translatedY);
             }
 
-        return translatedVertices;
+        return verticesTransladados;
     }
 
     void rotate(double angle) {
-        Point cg = centroid();
+        Point cg = centroide();
         double radians = angle * M_PI / 180.0;
-        int n = translatedVertices.size();
+        int n = verticesTransladados.size();
 
-        // Percorre cada vértice em translatedVertices usando índice
+        // Percorre cada vértice em verticesTransladados usando índice
         for (size_t i = 0; i < n; ++i) {
-            double x_new = (translatedVertices[i].x - cg.x) * cos(radians) - (translatedVertices[i].y - cg.y) * sin(radians) + cg.x;
-            double y_new = (translatedVertices[i].x - cg.x) * sin(radians) + (translatedVertices[i].y - cg.y) * cos(radians) + cg.y;
+            double x_new = (verticesTransladados[i].x - cg.x) * cos(radians) - (verticesTransladados[i].y - cg.y) * sin(radians) + cg.x;
+            double y_new = (verticesTransladados[i].x - cg.x) * sin(radians) + (verticesTransladados[i].y - cg.y) * cos(radians) + cg.y;
 
-            translatedVertices[i].x = x_new;
-            translatedVertices[i].y = y_new;
+            verticesTransladados[i].x = x_new;
+            verticesTransladados[i].y = y_new;
         }
     }
 
     void getYRange(double &yMin, double &yMax) const {
-        if (translatedVertices.empty()) return;
+        if (verticesTransladados.empty()) return;
 
-        yMin = translatedVertices[0].y;
-        yMax = translatedVertices[0].y;
+        yMin = verticesTransladados[0].y;
+        yMax = verticesTransladados[0].y;
 
-        int n = translatedVertices.size();
+        int n = verticesTransladados.size();
 
-        // Percorre os vértices em translatedVertices usando índice
+        // Percorre os vértices em verticesTransladados usando índice
         for (size_t i = 1; i < n; ++i) {
-            if (translatedVertices[i].y < yMin) yMin = translatedVertices[i].y;
-            if (translatedVertices[i].y > yMax) yMax = translatedVertices[i].y;
+            if (verticesTransladados[i].y < yMin) yMin = verticesTransladados[i].y;
+            if (verticesTransladados[i].y > yMax) yMax = verticesTransladados[i].y;
         }
     }
 
@@ -179,21 +178,21 @@ public:
         }
     }
 
-    void cortarPoligonal(const std::vector<Point> &translatedVertices, float &cortar)
+    void cortarPoligonal(const std::vector<Point> &verticesTransladados, float &cortar)
     {
         resultadoCorte.clear();
         areaComprimida.clear();
         areaTracionada.clear();
-        int nv = translatedVertices.size();
+        int nv = verticesTransladados.size();
         std::set<Point> pontosAdicionados; // Conjunto para rastrear pontos já adicionados
 
         for (int i = 0; i < nv; i++)
         {
-            int caso = verificarCaso(translatedVertices[i], translatedVertices[(i + 1) % nv], cortar);
+            int caso = verificarCaso(verticesTransladados[i], verticesTransladados[(i + 1) % nv], cortar);
 
             if (caso == 2)
             {
-                Point intersecao = calcularIntersecao(translatedVertices[i], translatedVertices[(i + 1) % nv], cortar);
+                Point intersecao = calcularIntersecao(verticesTransladados[i], verticesTransladados[(i + 1) % nv], cortar);
                 if (pontosAdicionados.insert(intersecao).second) // Adiciona se não existir
                 {
                     resultadoCorte.push_back(intersecao);
@@ -203,32 +202,104 @@ public:
             }
             if (caso == 3)
             {
-                if (pontosAdicionados.insert(translatedVertices[i]).second) // Adiciona se não existir
+                if (pontosAdicionados.insert(verticesTransladados[i]).second) // Adiciona se não existir
                 {
-                    resultadoCorte.push_back(translatedVertices[i]);
-                    areaComprimida.push_back(translatedVertices[i]);
+                    resultadoCorte.push_back(verticesTransladados[i]);
+                    areaComprimida.push_back(verticesTransladados[i]);
                 }
-                if (pontosAdicionados.insert(translatedVertices[(i + 1) % nv]).second) // Adiciona se não existir
+                if (pontosAdicionados.insert(verticesTransladados[(i + 1) % nv]).second) // Adiciona se não existir
                 {
-                    resultadoCorte.push_back(translatedVertices[(i + 1) % nv]);
-                    areaComprimida.push_back(translatedVertices[(i + 1) % nv]);
+                    resultadoCorte.push_back(verticesTransladados[(i + 1) % nv]);
+                    areaComprimida.push_back(verticesTransladados[(i + 1) % nv]);
                 }
             }
             if (caso == 4)
             {
-                if (pontosAdicionados.insert(translatedVertices[i]).second) // Adiciona se não existir
+                if (pontosAdicionados.insert(verticesTransladados[i]).second) // Adiciona se não existir
                 {
-                    resultadoCorte.push_back(translatedVertices[i]);
-                    areaTracionada.push_back(translatedVertices[i]);
+                    resultadoCorte.push_back(verticesTransladados[i]);
+                    areaTracionada.push_back(verticesTransladados[i]);
                 }
-                if (pontosAdicionados.insert(translatedVertices[(i + 1) % nv]).second) // Adiciona se não existir
+                if (pontosAdicionados.insert(verticesTransladados[(i + 1) % nv]).second) // Adiciona se não existir
                 {
-                    resultadoCorte.push_back(translatedVertices[(i + 1) % nv]);
-                    areaTracionada.push_back(translatedVertices[(i + 1) % nv]);
+                    resultadoCorte.push_back(verticesTransladados[(i + 1) % nv]);
+                    areaTracionada.push_back(verticesTransladados[(i + 1) % nv]);
                 }
             }
         }
     }
+};
+
+class Concreto{
+    public:
+    Poligono poligono;  
+    std::vector<Point> verticesComprimidos;
+    double fck, alfa_c, lambda, Epcu, n_conc, Epc2, fcd;
+
+    Concreto(const std::vector<Point>& vertices) {
+        poligono.setVertices(vertices);
+    }
+
+    void calcularAreaComprimida(float linhaDeCorte) {
+        poligono.verticesTransladados = poligono.translateToCG();
+        poligono.cortarPoligonal(poligono.verticesTransladados, linhaDeCorte);
+        verticesComprimidos = poligono.areaComprimida;
+    }
+
+    void definirParametrosConcreto(double fck) {
+        if (fck <= 50) {
+            alfa_c = 0.85;
+            lambda = 0.8;
+            Epcu = 3.5;  // Por mil Em milímetros
+            n_conc = 2;
+            Epc2 = 2;    // Por mil Em milímetros
+        } else {
+            alfa_c = 0.85 * (1 - (fck - 50) / 200);
+            lambda = 0.8 - (fck - 50) / 400;
+            Epcu = 2.6 + 35 * pow((90 - fck) / 100, 4);
+            n_conc = 1.4 + 23.4 * pow((90 - fck) / 100, 4);
+            Epc2 = 2 + 0.085 * pow(fck - 50, 0.53);
+            if (Epc2 > Epcu) {
+                Epc2 = Epcu;
+            }
+        }
+        fcd = fck / 1.4;  // Exemplo: gama_c definido como 1.4
+    }
+
+    std::pair<double,double> Nctr_Mctr(double fcd, double c1, double c2, double y, double mult_fcd) {
+        double Nctr = mult_fcd * fcd * (c1 * y + c2 * y * y / 2.0);
+        double Mctr = mult_fcd * fcd * (c1 * y * y / 2.0 + c2 * y * y * y / 3.0);
+        return {Nctr, Mctr};
+    }
+
+    std::pair<double,double> Nctp_Mctp(double fcd, double xEpc2, double n_conc, double c1, double c2, double y, double ymax, double xalfa, double mult_fcd) {
+        
+    }
+
+    std::pair<double, double> calcularNcMc(double EpA, double xalfa, double Epap, double d, const std::vector<Point>& verticesComprimidos, double vmax, double mult_fcd) {
+        double Nc = 0, Mc = 0;
+        double k = (Epap - EpA) / d / 1000.0;
+
+        
+
+        return { Nc * 1E3, Mc * 1E3 }; // Retorna em kN e kN*m
+    }
+};
+    
+
+class Aco{
+    public:
+    
+    void definirParametrosAco{
+        double fyk, fyd, Es, gama_s, Eps, Epyd;
+        fyk = 500; //MPa
+        gama_s = 1.15;
+        fyd = fyk/gama_s;
+        Es = 210; //GPa;
+    }
+
+    std::pair<double, double> calcularNpMp;
+
 };
 
 
@@ -239,26 +310,26 @@ int main() {
         {95,0}, {95, 25}, {70,45}, {70,170}, {120,178}, {120,190}
     }; 
 
-    Polygon polygon;
+    Poligono poligono;
 
     float linhaDeCorte = 100.0;
 
-    polygon.setVertices(collectedPoints);
+    poligono.setVertices(collectedPoints);
 
-    polygon.cortarPoligonal(polygon.translatedVertices, linhaDeCorte);
+    poligono.cortarPoligonal(poligono.verticesTransladados, linhaDeCorte);
 
     std::cout << "Pontos após o corte:" << std::endl;
-    for (const auto& p : polygon.resultadoCorte) {
+    for (const auto& p : poligono.resultadoCorte) {
         std::cout << "(" << p.x << ", " << p.y << ")" << std::endl;
     }
 
     std::cout << "\nÁrea Comprimida:" << std::endl;
-    for (const auto& p : polygon.areaComprimida) {
+    for (const auto& p : poligono.areaComprimida) {
         std::cout << "(" << p.x << ", " << p.y << ")" << std::endl;
     }
 
     std::cout << "\nÁrea Tracionada:" << std::endl;
-    for (const auto& p : polygon.areaTracionada) {
+    for (const auto& p : poligono.areaTracionada) {
         std::cout << "(" << p.x << ", " << p.y << ")" << std::endl;
     }
 

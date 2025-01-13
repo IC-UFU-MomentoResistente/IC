a62064c4056429603c14d8f99663ab74fccb4da6
Arthur Cunha Pena <arthurgepit2016@gmail.com>
2025-01-12 21:53:08 -0300

    12/01 - implementação do cálculo das alturas de deformação nessa branch

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include "raylib.h"
 #include "raymath.h"
@@ -19,20 +20,110 @@ struct Ponto
 	Ponto(float coordX, float coordY) : x(coordX), y(coordY) {}
 };
 
+struct VetoresAcimaAbaixo
+{
+	vector<Ponto> verticesAcima;
+	vector<Ponto> verticesAbaixo;
+};
+
 struct Poligonal
 {
 	vector<Ponto> verticesPoligonal;
+	vector<Ponto> verticesPoligonalVazada;
+	VetoresAcimaAbaixo vetoresAcimaAbaixo;
+	VetoresAcimaAbaixo vetoresAcimaAbaixoVazada;
+	float yMaximo;
+	float yMinimo;
+	float hSecao;
 };
 
-void renderizacaoVertices(const vector<Ponto>&);
-vector<Ponto> adicionaVertices(vector<Ponto> verticesColetados);
+struct Reforco
+{
+	vector<Ponto> barras;
+};
+
+struct ParametrosConcreto
+{
+	float fatorTensaoCompressao;
+	float epsUltimo;
+	float epsLimite;
+	float expTensaoCompressao;
+	float fcd;
+};
+
+struct AlturasDeformacao
+{
+	float hLN;
+	float hEpsLimite;
+	float hEpsUltimo;
+};
+
+struct SecaoComposta
+{
+	Poligonal secao;
+	Poligonal poligonoComprimido;
+	Reforco armadura;
+	ParametrosConcreto parametos;
+	AlturasDeformacao alturasDeformacao;
+};
+
+void renderizacaoPoligonal(vector<Ponto>, string, string);
+void renderizacaoBarras(vector<Ponto>, string);
+void exibirDadosSecaoComposta(SecaoComposta secao);
+vector<Ponto> adicionaVertices(vector<Ponto>);
+Ponto calculaIntersecao(Ponto, Ponto, float);
+VetoresAcimaAbaixo corteLinha(vector<Ponto>, float);
+float yMaximo(vector<Ponto>);
+float yMinimo(vector<Ponto>);
+ParametrosConcreto calculaParametrosConcreto(float, float);
+AlturasDeformacao calculaAlturasDeformacao(ParametrosConcreto, Poligonal, float, float);
 
 int main()
 {
+	// Declaração dos registros utilizados
 	Poligonal retangulo;
-	vector<Ponto> verticesColetados = { {-10, 20}, {-10, -20}, {10, -20}, {10, 20} };
+	Poligonal poligonoComprimido;
+	Reforco armadura;
+	ParametrosConcreto parametrosConcreto;
+	AlturasDeformacao alturasDeformacao;
+	SecaoComposta secaoComposta;
 
-	retangulo.verticesPoligonal = adicionaVertices(verticesColetados);
+	// Declaração e inicialização dos vértices
+	vector<Ponto> verticesColetados = { {-10, 20}, {-10, -20}, {10, -20}, {10, 20} };
+	vector<Ponto> verticesVazados = { {5,10}, {5, -10}, {-5, -10}, {-5,10} };
+	vector<Ponto> verticesBarras = { {-5,0}, {5,0} };
+
+	// Inicilização de poligonal (retangulo)
+	retangulo.verticesPoligonal = verticesColetados;
+	retangulo.verticesPoligonalVazada = verticesVazados;
+	armadura.barras = verticesBarras;
+
+	// Declaração e inicialização para os parâmetros do concreto
+	float fck = 30;
+	float gamaC = 1.4;
+	parametrosConcreto = calculaParametrosConcreto(fck, gamaC);
+
+	// Declaração e inicialização para as alturas de deformação para um conjunto de deformações
+	float eps1 = -1.1892;
+	float eps2 = 10.907;
+	retangulo.yMaximo = yMaximo(retangulo.verticesPoligonal);
+	retangulo.yMinimo = yMinimo(retangulo.verticesPoligonal);
+	retangulo.hSecao = retangulo.yMaximo - retangulo.yMinimo;
+	alturasDeformacao = calculaAlturasDeformacao(parametrosConcreto, retangulo, eps1, eps2);
+
+	// Declaração e inicialização para o corte nas alturas de inicialização
+	retangulo.vetoresAcimaAbaixo = corteLinha(retangulo.verticesPoligonal, alturasDeformacao.hLN);
+	poligonoComprimido.verticesPoligonal = retangulo.vetoresAcimaAbaixo.verticesAcima;
+	poligonoComprimido.vetoresAcimaAbaixo = corteLinha(poligonoComprimido.verticesPoligonal, alturasDeformacao.hEpsLimite);
+
+	// Declaração e inicialização do registro SecaoComposta
+	secaoComposta.secao = retangulo;
+	secaoComposta.poligonoComprimido = poligonoComprimido;
+	secaoComposta.armadura = armadura;
+	secaoComposta.parametos = parametrosConcreto;
+	secaoComposta.alturasDeformacao = alturasDeformacao;
+
+	exibirDadosSecaoComposta(secaoComposta);
 
 	// Initialization
 	//--------------------------------------------------------------------------------------
@@ -60,7 +151,24 @@ int main()
 			// inicia a janela no ImGui
 			ImGui::Begin("Secao Transversal", &janelaSecao);
 
-			renderizacaoVertices(retangulo.verticesPoligonal);
+			ImVec2 plotSize = ImGui::GetContentRegionAvail();
+
+			// inicialização do gráfico com os eixos
+			if (ImPlot::BeginPlot("Grafico", ImVec2(plotSize.x, plotSize.y), ImPlotFlags_Equal))
+			{
+				renderizacaoPoligonal(poligonoComprimido.verticesPoligonal, "vComprimido", "pComprimido");
+				renderizacaoPoligonal(poligonoComprimido.vetoresAcimaAbaixo.verticesAcima, "vRetangulo", "pRetangulo");
+				renderizacaoPoligonal(poligonoComprimido.vetoresAcimaAbaixo.verticesAbaixo, "vParabola", "pParabola");
+				// renderizacaoPoligonal(retangulo.verticesPoligonal, "Vertices1", "Retangulo");
+				// renderizacaoBarras(armadura.barras, "Barras");
+				// renderizacaoPoligonal(retangulo.verticesPoligonalVazada, "Vertices2", "RetanguloVazado");
+				// renderizacaoPoligonal(retangulo.vetoresAcimaAbaixo.verticesAcima, "vAcima", "Acima");
+				// renderizacaoPoligonal(retangulo.vetoresAcimaAbaixo.verticesAbaixo, "vAbaixo", "Abaixo");
+				// renderizacaoPoligonal(retangulo.vetoresAcimaAbaixoVazada.verticesAcima, "v2Acima", "2Acima");
+				// renderizacaoPoligonal(retangulo.vetoresAcimaAbaixoVazada.verticesAbaixo, "v2Abaico", "2Abaixo");
+			}
+
+			ImPlot::EndPlot();
 
 			// finaliza a janela no ImGui
 			ImGui::End();
@@ -83,60 +191,137 @@ int main()
 	return 0;
 }
 
-void renderizacaoVertices(const vector<Ponto>& vertices)
+void renderizacaoPoligonal(vector<Ponto> vertices, string nomeVertices, string nomePoligonal)
 {
-	// Obter o tamanho disponível para o gráfico
-	ImVec2 plotSize = ImGui::GetContentRegionAvail();
-
-	// inicialização do gráfico com os eixos
-	if (ImPlot::BeginPlot("Gráfico", ImVec2(plotSize.x, plotSize.y), ImPlotFlags_Equal))
+	// verificação se o vetor está vazio, prossegue se não
+	if (!vertices.empty())
 	{
-		// verificação se o vetor está vazio, prossegue se não
-		if (!vertices.empty())
+		// vetores temporários para usar no PlotScatter
+		// inicialização com o tamanho do vetor do parâmetro da função
+		// inicialização do tipo vector<float> nome(tamanho)
+		vector<float> xTemp(vertices.size());
+		vector<float> yTemp(vertices.size());
+
+		// alimentação dos vetores temporários
+		for (size_t i = 0; i < vertices.size(); i++)
 		{
-			// vetores temporários para usar no PlotScatter
-			// inicialização com o tamanho do vetor do parâmetro da função
-			// inicialização do tipo vector<float> nome(tamanho)
-			vector<float> xTemp(vertices.size());
-			vector<float> yTemp(vertices.size());
+			xTemp[i] = vertices[i].x;
+			yTemp[i] = vertices[i].y;
+		}
+
+		// desenho dos pontos
+		// metodo data() para referenciar como ponteiro 
+		// static_cast é uma conversão de tipo size_t (unsigned inteiro) para int
+		// não está relacionada a variáveis estáticas
+		ImPlot::PlotScatter(nomeVertices.c_str(), xTemp.data(), yTemp.data(), static_cast<int>(vertices.size()));
+
+		// verificação se há três pontos para fechar uma poligonal
+		if (vertices.size() > 2)
+		{
+			vector<float> xTempAresta(vertices.size() + 1);
+			vector<float> yTempAresta(vertices.size() + 1);
 
 			// alimentação dos vetores temporários
 			for (size_t i = 0; i < vertices.size(); i++)
 			{
-				xTemp[i] = vertices[i].x;
-				yTemp[i] = vertices[i].y;
+				xTempAresta[i] = vertices[i].x;
+				yTempAresta[i] = vertices[i].y;
 			}
 
-			// desenho dos pontos
-			// metodo data() para referenciar como ponteiro 
-			// static_cast é uma conversão de tipo size_t (unsigned inteiro) para int
-			// não está relacionada a variáveis estáticas
-			ImPlot::PlotScatter("Vertices", xTemp.data(), yTemp.data(), static_cast<int>(vertices.size()));
+			// fecha a poligonal ao fazer com que o último elemento seja igual ao primeiro
+			xTempAresta[vertices.size()] = vertices[0].x;
+			yTempAresta[vertices.size()] = vertices[0].y;
 
-			// verificação se há três pontos para fechar uma poligonal
-			if (vertices.size() > 2)
-			{
-				vector<float> xTempAresta(vertices.size() + 1);
-				vector<float> yTempAresta(vertices.size() + 1);
+			// desenho das arestas
+			ImPlot::PlotLine(nomePoligonal.c_str(), xTempAresta.data(), yTempAresta.data(), static_cast<int>(xTempAresta.size()));
+		}
+	}
+}
 
-				// alimentação dos vetores temporários
-				for (size_t i = 0; i < vertices.size(); i++)
-				{
-					xTempAresta[i] = vertices[i].x;
-					yTempAresta[i] = vertices[i].y;
-				}
+void renderizacaoBarras(vector<Ponto> vertices, string nomeVerticesBarras)
+{
+	// verificação se o vetor está vazio, prossegue se não
+	if (!vertices.empty())
+	{
+		// vetores temporários para usar no PlotScatter
+		// inicialização com o tamanho do vetor do parâmetro da função
+		// inicialização do tipo vector<float> nome(tamanho)
+		vector<float> xTemp(vertices.size());
+		vector<float> yTemp(vertices.size());
 
-				// fecha a poligonal ao fazer com que o último elemento seja igual ao primeiro
-				xTempAresta[vertices.size()] = vertices[0].x;
-				yTempAresta[vertices.size()] = vertices[0].y;
-
-				// desenho das arestas
-				ImPlot::PlotLine("Poligonal", xTempAresta.data(), yTempAresta.data(), static_cast<int>(xTempAresta.size()));
-			}
+		// alimentação dos vetores temporários
+		for (size_t i = 0; i < vertices.size(); i++)
+		{
+			xTemp[i] = vertices[i].x;
+			yTemp[i] = vertices[i].y;
 		}
 
-		ImPlot::EndPlot();
+		// desenho dos pontos
+		// metodo data() para referenciar como ponteiro 
+		// static_cast é uma conversão de tipo size_t (unsigned inteiro) para int
+		// não está relacionada a variáveis estáticas
+		ImPlot::PlotScatter(nomeVerticesBarras.c_str(), xTemp.data(), yTemp.data(), static_cast<int>(vertices.size()));
 	}
+}
+
+void exibirDadosSecaoComposta(SecaoComposta secaoComposta)
+{
+	cout << "--------------------------------------------\n";
+	cout << "Secao: ";
+
+	cout << "\nVertices: ";
+	for (size_t i = 0; i < secaoComposta.secao.verticesPoligonal.size(); i++)
+	{
+		cout << "("
+			<< secaoComposta.secao.verticesPoligonal[i].x << ","
+			<< secaoComposta.secao.verticesPoligonal[i].y << ") ";
+	}
+
+	cout << "\nVertices comprimidos: ";
+	for (size_t i = 0; i < secaoComposta.poligonoComprimido.verticesPoligonal.size(); i++)
+	{
+		cout << "("
+			<< secaoComposta.poligonoComprimido.verticesPoligonal[i].x << ","
+			<< secaoComposta.poligonoComprimido.verticesPoligonal[i].y << ") ";
+	}
+
+	cout << "\nVertices Tensao Retangulo: ";
+	for (size_t i = 0; i < secaoComposta.poligonoComprimido.vetoresAcimaAbaixo.verticesAcima.size(); i++)
+	{
+		cout << "("
+			<< secaoComposta.poligonoComprimido.vetoresAcimaAbaixo.verticesAcima[i].x << ","
+			<< secaoComposta.poligonoComprimido.vetoresAcimaAbaixo.verticesAcima[i].y << ") ";
+	}
+
+	cout << "\nVertices Tensao Parabola: ";
+	for (size_t i = 0; i < secaoComposta.poligonoComprimido.vetoresAcimaAbaixo.verticesAbaixo.size(); i++)
+	{
+		cout << "("
+			<< secaoComposta.poligonoComprimido.vetoresAcimaAbaixo.verticesAbaixo[i].x << ","
+			<< secaoComposta.poligonoComprimido.vetoresAcimaAbaixo.verticesAbaixo[i].y << ") ";
+	}
+
+	cout << endl;
+	cout << "Altura Secao: " << secaoComposta.secao.hSecao << endl;
+	cout << "yMaximo: " << secaoComposta.secao.yMaximo << endl;
+	cout << "yMinimo: " << secaoComposta.secao.yMinimo << endl;
+	cout << "--------------------------------------------\n";
+
+	cout << "--------------------------------------------\n";
+	cout << "Parametros Concreto: \n";
+	cout << "fcd: " << secaoComposta.parametos.fcd << endl;
+	cout << "Fator mult: " << secaoComposta.parametos.fatorTensaoCompressao << endl;
+	cout << "Exp: " << secaoComposta.parametos.expTensaoCompressao << endl;
+	cout << "Eps Ultimo: " << secaoComposta.parametos.epsUltimo << endl;
+	cout << "Eps Limite " << secaoComposta.parametos.epsLimite << endl;
+	cout << "--------------------------------------------\n";
+
+	cout << "--------------------------------------------\n";
+	cout << "Alturas Deformacao: \n";
+	cout << "hLN: " << secaoComposta.alturasDeformacao.hLN << endl;
+	cout << "hEpsLimite: " << secaoComposta.alturasDeformacao.hEpsLimite << endl;
+	cout << "hEpsUltimo: " << secaoComposta.alturasDeformacao.hEpsUltimo << endl;
+	cout << "--------------------------------------------\n";
 }
 
 vector<Ponto> adicionaVertices(vector<Ponto> verticesColetados)
@@ -159,4 +344,122 @@ vector<Ponto> adicionaVertices(vector<Ponto> verticesColetados)
 
 	// retorno do vetor temporário
 	return verticesTemp;
+}
+
+
+// Função auxiliar para calcular o ponto de interseção entre um segmento e a linha horizontal
+Ponto calculaIntersecao(Ponto p1, Ponto p2, float yCorte) {
+	float dx = p2.x - p1.x;
+	float dy = p2.y - p1.y;
+	float t = (yCorte - p1.y) / dy;  // Interpolação linear
+	return { p1.x + t * dx, yCorte };
+}
+
+// Função para cortar a seção transversal
+VetoresAcimaAbaixo corteLinha(vector<Ponto> vertices, float yCorte)
+{
+	VetoresAcimaAbaixo resultado;
+
+	for (size_t i = 0; i < vertices.size(); ++i) {
+		Ponto atual = vertices[i];
+		Ponto proximo = vertices[(i + 1) % vertices.size()];  // Laço fechado
+
+		bool atualAcima = atual.y > yCorte;
+		bool proximoAcima = proximo.y > yCorte;
+
+		if (atual.y == yCorte)
+		{
+			resultado.verticesAcima.push_back(atual);
+			resultado.verticesAbaixo.push_back(atual);
+		}
+		else if (atualAcima)
+		{
+			resultado.verticesAcima.push_back(atual);
+		}
+		else
+		{
+			resultado.verticesAbaixo.push_back(atual);
+		}
+
+		// Detectar interseção
+		if (atualAcima != proximoAcima)
+		{
+			Ponto intersecao = calculaIntersecao(atual, proximo, yCorte);
+			resultado.verticesAcima.push_back(intersecao);
+			resultado.verticesAbaixo.push_back(intersecao);
+		}
+	}
+
+	return resultado;
+}
+
+float yMaximo(vector<Ponto> verticesPoligonal)
+{
+	if (verticesPoligonal.empty())
+		throw invalid_argument("O vetor de vertices está vazio.");
+
+	float yMax = verticesPoligonal[0].y;
+
+	for (size_t i = 0; i < verticesPoligonal.size(); i++)
+	{
+		if (verticesPoligonal[i].y > yMax) yMax = verticesPoligonal[i].y;
+	}
+
+	return yMax;
+}
+
+float yMinimo(vector<Ponto> verticesPoligonal)
+{
+	if (verticesPoligonal.empty())
+		throw invalid_argument("O vetor de vertices está vazio.");
+
+	float yMin = verticesPoligonal[0].y;
+
+	for (size_t i = 0; i < verticesPoligonal.size(); i++)
+	{
+		if (verticesPoligonal[i].y < yMin) yMin = verticesPoligonal[i].y;
+	}
+
+	return yMin;
+}
+
+ParametrosConcreto calculaParametrosConcreto(float fck, float gamaC)
+{
+	ParametrosConcreto parametros;
+
+	if (fck <= 50)
+	{
+		parametros.fcd = fck / gamaC;
+		parametros.fatorTensaoCompressao = 0.85;
+		parametros.expTensaoCompressao = 2;
+		parametros.epsUltimo = 3.5;
+		parametros.epsLimite = 2;
+	}
+	else
+	{
+		parametros.fcd = fck / gamaC;
+		parametros.fatorTensaoCompressao = 0.85 * (1 - (fck - 50) / 200);
+		parametros.expTensaoCompressao = 2.6 + 35 * pow((90 - fck) / 100, 4);
+		parametros.epsUltimo = 1.4 + 23.4 * pow((90 - fck) / 100, 4);
+		parametros.epsLimite = 2 + 0.085 * pow(fck - 50, 0.53);
+
+		if (parametros.epsLimite > parametros.epsUltimo)
+		{
+			parametros.epsLimite = parametros.epsUltimo;
+		}
+	}
+
+	return parametros;
+}
+
+AlturasDeformacao calculaAlturasDeformacao(ParametrosConcreto parametros, Poligonal secao, float eps1, float eps2)
+{
+	AlturasDeformacao alturasTemp;
+	float k = ((eps2 / 1000) - (eps1 / 1000)) * secao.hSecao;
+
+	alturasTemp.hLN = secao.yMaximo - (((-eps1 / 1000) / ((eps2 / 1000) - (eps1 / 1000))) * secao.hSecao);
+	alturasTemp.hEpsLimite = secao.yMaximo - ((((-parametros.epsLimite / 1000) - (eps1 / 1000)) / ((eps2 / 1000) - (eps1 / 1000))) * secao.hSecao);
+	alturasTemp.hEpsUltimo = secao.yMaximo - ((((-parametros.epsUltimo / 1000) - (eps1 / 1000)) / ((eps2 / 1000) - (eps1 / 1000))) * secao.hSecao);
+
+	return alturasTemp;
 }

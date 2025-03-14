ddad8b5b7a160148fa79105b83d1c32acf251916
Arthur Cunha Pena <arthurgepit2016@gmail.com>
2025-03-14 14:11:03 -0300

    feat(main): add sectionWindow, reinforcementWindow, crossSectionPlotWindow, and ImGui's showDemoWindow

--- src/main.cpp
+++ src/main.cpp
@@ -7,104 +7,225 @@
 
 #include "Point.h"
 #include "Polygon.h"
+#include "Reinforcement.h"
 #include "AppView.h"
 
 using std::cout;
+using std::endl;
 
 int main()
 {
-    Polygon polygon;
-    AppView view;
+	Polygon polygon;
+	Reinforcement reinforcement;
+	AppView view;
 
-    int screenWidth = 1280;
-    int screenHeight = 800;
+	int screenWidth = 1280;
+	int screenHeight = 800;
 
-    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
-    InitWindow(screenWidth, screenHeight, "IC - FCO - Momentos Resistentes");
-    SetTargetFPS(60);
-    rlImGuiSetup(true);
-    ImPlot::CreateContext();
+	SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
+	InitWindow(screenWidth, screenHeight, "IC - FCO - Momentos Resistentes");
+	SetTargetFPS(60);
+	rlImGuiSetup(true);
 
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(DARKGRAY);
-        rlImGuiBegin();
+	ImGui::GetStyle().AntiAliasedFill = true;
+	ImGui::GetStyle().AntiAliasedLines = true;
 
-        if (ImGui::BeginMainMenuBar())
-        {
-            if (ImGui::BeginMenu("Arquivo"))
-            {
-                ImGui::MenuItem("Salvar");
-                ImGui::MenuItem("Carregar");
-                ImGui::EndMenu();
-            }
+	ImPlot::CreateContext();
 
-            if (ImGui::BeginMenu("Sobre"))
-            {
-                ImGui::Text("Versao 1.0");
-                ImGui::EndMenu();
-            }
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+		ClearBackground(DARKGRAY);
+		rlImGuiBegin();
 
-            ImGui::EndMainMenuBar();
-        }
+		if (ImGui::BeginMainMenuBar())
+		{
+			if (ImGui::BeginMenu("Arquivo"))
+			{
+				ImGui::MenuItem("Salvar");
+				ImGui::MenuItem("Carregar");
+				ImGui::EndMenu();
+			}
 
-        ImGui::Begin("Secao Transversal");
+			if (ImGui::BeginMenu("Sobre"))
+			{
+				ImGui::Text("Versao 1.0");
+				ImGui::EndMenu();
+			}
 
-        static double x, y;
-        ImGui::InputDouble("x (cm)", &x);
-        ImGui::InputDouble("y (cm)", &y);
+			ImGui::EndMainMenuBar();
+		}
 
-        if (ImGui::Button("Adicionar Ponto"))
-        {
-            polygon.addVertice(x, y);
-        }
+		bool sectionWindow = true;
 
-        ImGui::SameLine();
+		if (sectionWindow)
+		{
+			ImGui::Begin("Secao Transversal");
 
-        if (ImGui::Button("Remover Ponto"))
-        {
-            if(!polygon.getPolygonVertices().empty())
-                polygon.removeLastVertice();
-        }
+			static double coordXPolygon, coordYPolygon;
+			ImGui::Text("Vertices");
+			ImGui::InputDouble("x (cm)", &coordXPolygon);
+			ImGui::InputDouble("y (cm)", &coordYPolygon);
 
-        ImGui::SameLine();
+			if (ImGui::Button("Adicionar Ponto"))
+			{
+				polygon.addVertice(coordXPolygon, coordYPolygon);
+			}
 
-        if (ImGui::Button("Calcular parametros"))
-        {
-            polygon.computeArea();
-            polygon.computeCentroid();
-            polygon.computeMaxCoordY();
-            polygon.computeMinCoordY();
-            polygon.computeHeight();
-        }
+			ImGui::SameLine();
 
-        ImGui::Text("Area: %.2f", polygon.getPolygonArea());
-        ImGui::Text("MaxY: %.2f", polygon.getMaxY());
-        ImGui::Text("MinY: %.2f", polygon.getMinY());
-        ImGui::Text("Height: %.2f", polygon.getMaxY());
-        ImGui::Text("CG: %.2f, %.2f", polygon.getGeometricCenter().getX(), polygon.getGeometricCenter().getY());
+			if (ImGui::Button("Remover Ponto"))
+			{
+				if (!polygon.getPolygonVertices().empty())
+					polygon.removeLastVertice();
+			}
 
-        ImVec2 plotSize = ImGui::GetContentRegionAvail();
+			ImGui::SameLine();
 
-        // inicialização do gráfico com os eixos
-        if (ImPlot::BeginPlot("Grafico", ImVec2(plotSize.x, plotSize.y), ImPlotFlags_Equal))
-        {
-            if (polygon.getPolygonVertices().size() > 2)
-                view.renderPolygon(polygon.getPolygonVertices(), "Vertices", "Polygon");
-        }
+			if (ImGui::Button("Calcular parametros"))
+			{
+				polygon.computeArea();
+				polygon.computeCentroid();
+				polygon.computeMaxCoordY();
+				polygon.computeMinCoordY();
+				polygon.computeHeight();
+			}
 
-        ImPlot::EndPlot();
+			ImGui::SameLine();
 
-        // finaliza a janela no ImGui
-        ImGui::End();
+			if (ImGui::Button("Transladar"))
+			{
+				polygon.translateToCentroid();
+				reinforcement.translateToCentroidPolygon(polygon.getGeometricCenter());
+			}
 
-        rlImGuiEnd();
-        EndDrawing();
-    }
+			ImGui::SameLine();
 
-    ImPlot::DestroyContext();
-    rlImGuiShutdown();
-    CloseWindow();
+			if (ImGui::Button("Rotacionar"))
+			{
+				polygon.rotateAroundCentroid(10);
+				reinforcement.rotateAroundCentroidPolygon(10, polygon.getGeometricCenter());
+			}
 
-    return 0;
+			ImGui::Text("Area: %.2f", polygon.getPolygonArea());
+			ImGui::Text("MaxY: %.2f", polygon.getMaxY());
+			ImGui::Text("MinY: %.2f", polygon.getMinY());
+			ImGui::Text("Height: %.2f", polygon.getMaxY());
+			ImGui::Text("CG: %.2f, %.2f", polygon.getGeometricCenter().getX(), polygon.getGeometricCenter().getY());
+			ImGui::Text("Vet0: %.2f, %.2f", polygon.getVet0X(), polygon.getVet0Y());
+
+			// finaliza a janela no ImGui
+			ImGui::End();
+		}
+
+		bool reinforcementWindow = true;
+
+		static int barMode, numBar = 0;
+		static double coordXBar, coordYBar, diameterBar, coordXiBar, coordXfBar, coordYiBar, coordYfBar;
+
+		if (reinforcementWindow)
+		{
+			ImGui::Begin("Entrada de dados: Armadura Passiva");
+			ImGui::RadioButton("Uma barra", &barMode, 0);
+			ImGui::RadioButton("Linha de barras", &barMode, 1);
+
+
+			if (barMode == 0)
+			{
+				ImGui::InputDouble("Diametro da barra (mm)", &diameterBar);
+				ImGui::InputDouble("x (cm)", &coordXBar);
+				ImGui::InputDouble("y (cm)", &coordYBar);
+
+				if (diameterBar < 0)
+					diameterBar = 0;
+
+				if (ImGui::Button("Adicionar"))
+					reinforcement.addReinforcement(coordXBar, coordYBar, diameterBar);
+
+				ImGui::SameLine();
+
+				if (ImGui::Button("Remover"))
+				{
+					if (!reinforcement.getReinforcement().empty())
+						reinforcement.removeLastBar();
+				}
+			}
+
+			if (barMode == 1)
+			{
+				ImGui::InputInt("Numero de barras na linha", &numBar);
+				ImGui::InputDouble("Diametro das barras (mm)", &diameterBar);
+				ImGui::InputDouble("xi (cm)", &coordXiBar);
+				ImGui::InputDouble("xf (cm)", &coordXfBar);
+				ImGui::InputDouble("yi (cm)", &coordYiBar);
+				ImGui::InputDouble("yf (cm)", &coordYfBar);
+
+				if (diameterBar < 0)
+					diameterBar = 0;
+
+				if (numBar < 2)
+					numBar = 2;
+
+				double stepX = (coordXfBar - coordXiBar) / (numBar - 1);
+				double stepY = (coordYfBar - coordYiBar) / (numBar - 1);
+
+				if (ImGui::Button("Adicionar"))
+				{
+					for (int i = 0; i < numBar; i++)
+					{
+						double coordX = coordXiBar + stepX * i;
+						double coordY = coordYiBar + stepY * i;
+
+						reinforcement.addReinforcement(coordX, coordY, diameterBar);
+					}
+				}
+
+				ImGui::SameLine();
+
+				if (ImGui::Button("Remover"))
+				{
+					if (!reinforcement.getReinforcement().empty())
+						reinforcement.removeLastBar();
+				}
+			}
+
+			ImGui::End();
+		}
+
+		bool crossSectionPlotWindow = true;
+
+		if (crossSectionPlotWindow)
+		{
+			ImGui::Begin("Grafico da Secao Transversal");
+
+			ImVec2 plotSize = ImGui::GetContentRegionAvail();
+
+			// inicialização do gráfico com os eixos
+			if (ImPlot::BeginPlot("Grafico", ImVec2(plotSize.x, plotSize.y), ImPlotFlags_Equal))
+			{
+				if (polygon.getPolygonVertices().size() > 2)
+				{
+					view.renderPolygon(polygon.getPolygonVertices(), "Vertices", "Polygon");
+					view.renderReinforcement(reinforcement.getReinforcement(), "Barras");
+				}
+			}
+
+			ImPlot::EndPlot();
+
+			ImGui::End();
+		}
+
+		bool showDemoWindow = true;
+
+		if (showDemoWindow)
+			ImGui::ShowDemoWindow(&showDemoWindow);
+
+		rlImGuiEnd();
+		EndDrawing();
+	}
+
+	ImPlot::DestroyContext();
+	rlImGuiShutdown();
+	CloseWindow();
+
+	return 0;
 }
